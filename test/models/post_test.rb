bce6d6f8645fd9fe276cf8c040cc84e46f37feec
sleepingfrog <57852015+sleepingfrog@users.noreply.github.com>
2023-08-03 13:45:59 +0900

    same validation

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -4,4 +4,20 @@ class PostTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test 'validなpost' do
+    post = Post.new(title: 'title')
+
+    assert post.valid?
+    assert_difference("Post.count", 1) do
+      post.save
+    end
+  end
+
+  test 'title は必須' do
+    post = Post.new(title: '')
+
+    assert post.invalid?
+    assert post.errors.details[:title].present?
+  end
 end
